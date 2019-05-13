7158c72b3753fb293ae68cbe5fe0c263947e1fc7
Guillaume97490 <techer.guillaume97490@gmail.com>
2019-05-13 21:44:24 +0400

    Premier commit

---
+++ js/app.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        usernameTmp : "",
+        username : "",
+        msg : "",
+        msgTmp :"",
+        msgDate:"",
+        listeMsgs:[],
+    },
+  })
+  
